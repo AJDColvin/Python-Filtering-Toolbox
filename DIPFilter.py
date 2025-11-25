c97c125684ca551bf6d772ce850615560ad3ea23
AJDColvin <ajdc21@bath.ac.uk>
2025-11-25 12:15:58 +0000

    Median Filter implemented

--- DIPFilter.py
+++ DIPFilter.py
@@ -2,16 +2,25 @@
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 
 class DIPFilters:
     def __init__(self, image_path):
         self.image_path = image_path
         self.img = mpimg.imread(self.image_path)
+        self.img_filtered = self.img
     
     def display(self):
         plt.figure()
         plt.imshow(self.img, cmap="gray")
+    
+    def col2greyscale(self):
+        if self.img.ndim == 2:
+            print("Image is already greyscale.")
+        else:
+            rgb_weights = np.array([0.299, 0.587, 0.114])
+            self.img = np.dot(self.img[..., :3], rgb_weights)
     
     def crudeMeanFilter(self, window_size):
         
@@ -40,6 +49,9 @@ class DIPFilters:
     
     def EfficientMeanFilter(self, window_size):
         
+        # Attempt at separability, ie one horizontal window
+        # Then one vertical window, to create 2D window
+        
         size = int((window_size-1)/2)
         window_area = window_size*window_size
         
@@ -58,22 +70,190 @@ class DIPFilters:
         
         plt.figure()
         plt.imshow(blurredImg, cmap="gray")
-                       
+        
+    def SeparableMeanFilter(self, window_size):
+        
+        #TODO: deal width the averaging of 0s in the edge
+        
+        size = int((window_size-1)/2)
+        window_area = window_size*window_size
+        padded_img = np.pad(self.img, pad_width=size, mode='constant')
+        
+        img_height = self.img.shape[0]
+        img_width = self.img.shape[1]
+        
+        intermediateArray = np.zeros_like(padded_img)
+        
+        # Horizontal filter pass
+        for row_index in range(size, img_height+size):
+            # print("START OF ROW")
+            for pixel_index in range(size, img_width+size):
+                window = padded_img[row_index, (pixel_index-size):(pixel_index+size+1)]
+                # print('Window', window)
+                total = np.sum(window)
+                new_pixel = total/window_area
+                intermediateArray[row_index, pixel_index] = new_pixel
+            
+        blurredImg = np.zeros_like(intermediateArray)
+        
+        # Vertical filter pass
+        for row_index in range(size, img_height+size):
+            for pixel_index in range(size, img_width+size):
+                window = intermediateArray[row_index-size:row_index+size+1, pixel_index]
+                total = np.sum(window)
+                new_pixel = total/window_area
+                blurredImg[row_index, pixel_index] = new_pixel 
         
         
+        slicedArray = blurredImg[size:img_height+size, size:img_width+size]
+        self.img_filtered = slicedArray
+        
+        plt.figure()
+        plt.title("Separable Mean Filter")
+        plt.imshow(slicedArray, cmap="gray")  
+
+    def GaussianFilter(self,  sd):
+        # Window size calculated from standard deviation
+        # = 2 * ceil(3*sd) + 1
+        
+        size = math.ceil(3*sd)
+        window_size = 2*size + 1
+        window_area = window_size*window_size
+        
+        gauss = lambda x : math.exp(-((x**2)/(2*sd**2)))
+        h_window = np.array([gauss(x) for x in range(-size, size+1)])
+        v_window = np.transpose(h_window)
+
+        padded_img = np.pad(self.img, pad_width=size, mode='constant')
+        
+        img_height = self.img.shape[0]
+        img_width = self.img.shape[1]
+        
+        intermediateArray = np.zeros_like(padded_img)
+        
+        # Horizontal filter pass
+        for row_index in range(size, img_height+size):
+            # print("START OF ROW")
+            for pixel_index in range(size, img_width+size):
+                window = padded_img[row_index, (pixel_index-size):(pixel_index+size+1)]
+                total = np.dot(window, h_window)
+                new_pixel = total/window_area
+                intermediateArray[row_index, pixel_index] = new_pixel
+            
+        blurredImg = np.zeros_like(intermediateArray)
+        
+        # Vertical filter pass
+        for row_index in range(size, img_height+size):
+            for pixel_index in range(size, img_width+size):
+                window = intermediateArray[row_index-size:row_index+size+1, pixel_index]
+                total = np.dot(window, v_window)
+                new_pixel = total/window_area
+                blurredImg[row_index, pixel_index] = new_pixel 
+        
+        
+        slicedArray = blurredImg[size:img_height+size, size:img_width+size]
+        self.img_filtered = slicedArray
+        
+        plt.figure()
+        plt.title("Gaussian Filter")
+        plt.imshow(slicedArray, cmap="gray")
+
+    def MedianFilter(self, window_size):
+        
+        def findMedian(hist, median_idx):
+            # Function for finding the median from a histogram
+            counter = 0 
+            for pixel in range(256):
+                counter += hist[pixel]
+                if counter >= median_idx:
+                    return pixel
+            return 0
+        
+        # Discretise image for histogram sort
+        img_disc = np.round(self.img*255)
+        
+        size = int((window_size-1)/2)
+        window_area = window_size*window_size
+        padded_img_disc = np.pad(img_disc, pad_width=size, mode='constant')
+        
+        img_height = self.img.shape[0]
+        img_width = self.img.shape[1]
+        
+        blurredImg = np.zeros_like(padded_img_disc)
+        
+        histogram = [0]*256
+        median_idx = (window_area//2)+1
+        
+        # Main loop through image
+        for row_index in range(size, img_height+size):
+            
+            # Initialise histogram for row
+            histogram = [0]*256
+            pixel_index = size
+            for hist_row_index in range(-size, size+1):
+                for hist_pixel_index in range(-size, size+1):
+                    pixel_val = padded_img_disc[row_index + hist_row_index, pixel_index + hist_pixel_index]
+                    histogram[int(pixel_val)] += 1
+            
+            # Find the median value from hist just made
+            blurredImg[row_index, pixel_index] = findMedian(histogram, median_idx)
+            
+            # Update histrogram for rest of pixels in row
+            for pixel_index in range(size+1, img_width+size):
+                old_col = pixel_index - size - 1
+                new_col = pixel_index + size
+                
+                for col_row_index in range(-size, size+1):
+                    
+                    # Remove pixel values from hist from column that just left
+                    old_pixel_val = padded_img_disc[row_index + col_row_index, old_col]
+                    histogram[int(old_pixel_val)] -= 1
+                    
+                    # Add pixel values to hist from column that just joined
+                    new_pixel_val = padded_img_disc[row_index + col_row_index, new_col]
+                    histogram[int(new_pixel_val)] += 1
+            
+                # Find median from updated hisogram
+                blurredImg[row_index, pixel_index] = findMedian(histogram, median_idx)
+        
+        # Slice off padding        
+        slicedArray = blurredImg[size:img_height+size, size:img_width+size]
+        self.img_filtered = slicedArray
+            
+        plt.figure()
+        plt.title("Median Filter")
+        plt.imshow(slicedArray, cmap="gray")          
+ 
+            
+        
+           
+        
+        
+        
+        
+
+
+        
+
     
+if __name__ == '__main__':
+    NZfilter = DIPFilters('Images/NZjers1.png')
+    NZfilter.display()
+    # NZfilter.crudeMeanFilter(7)
+    # NZfilter.EfficientMeanFilter(7)
+    NZfilter.SeparableMeanFilter(5)
+    NZfilter.GaussianFilter(4)
+    NZfilter.MedianFilter(5)
 
-    def GaussianFilter(self, window_size):
-        pass
+
+    # CarWindow = DIPFilters('Images/carwindow.jpg')
+    # CarWindow.display()
+    # CarWindow.col2greyscale()
+    # CarWindow.display()
+    # CarWindow.SeparableMeanFilter(150)
 
 
-        
 
-    
 
-NZfilter = DIPFilters('Images/NZjers1.png')
-NZfilter.display()
-NZfilter.crudeMeanFilter(7)
-NZfilter.EfficientMeanFilter(7)
 
 
